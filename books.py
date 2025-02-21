a7510bcfbc08788501f87e217975c3a37b1e50d5
Nimira43 <n.rabone72@aol.com>
2025-02-21 22:21:28 +0000

    Simplified code.

--- books.py
+++ books.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
 app = FastAPI()
 
 BOOKS = [
-    {'title': 'The Lord of the Rings', 'author': 'JRR Tolkien', 'category': 'Fantasy'},
-    {'title': 'Nineteen Eighty-Four', 'author': 'George Orwell', 'category': 'Dystopian'},
-    {'title': 'The Lion, the Witch and the Wardrobe', 'author': 'CS Lewis', 'category': 'Fantasy'},
-    {'title': 'Dune', 'author': 'Frank Herbert', 'category': 'Science Fiction'},
-    {'title': 'The Hobbit', 'author': 'JRR Tolkien', 'category': 'Fantasy'},
-    {'title': 'War and Peace', 'author': 'Leo Tolstoy', 'category': 'Historical'},
-    {'title': 'Wuthering Heights', 'author': 'Emily Brontë', 'category': 'Gothic'},
-    {'title': 'The Odyssey', 'author': 'Homer', 'category': 'Epic'},
-    {'title': '2001: A Space Odyssey', 'author': 'Arthur C. Clarke', 'category': 'Science Fiction'},
+  {'title': 'The Lord of the Rings', 'author': 'JRR Tolkien', 'category': 'Fantasy'},
+  {'title': 'Nineteen Eighty-Four', 'author': 'George Orwell', 'category': 'Dystopian'},
+  {'title': 'The Lion, the Witch and the Wardrobe', 'author': 'CS Lewis', 'category': 'Fantasy'},
+  {'title': 'Dune', 'author': 'Frank Herbert', 'category': 'Science Fiction'},
+  {'title': 'The Hobbit', 'author': 'JRR Tolkien', 'category': 'Fantasy'},
+  {'title': 'War and Peace', 'author': 'Leo Tolstoy', 'category': 'Historical'},
+  {'title': 'Wuthering Heights', 'author': 'Emily Brontë', 'category': 'Gothic'},
+  {'title': 'The Odyssey', 'author': 'Homer', 'category': 'Epic'},
+  {'title': '2001: A Space Odyssey', 'author': 'Arthur C. Clarke', 'category': 'Science Fiction'},
 ]
 
 @app.get('/books')
@@ -23,4 +23,3 @@ async def read_book(book_title: str):
   for book in BOOKS:
     if book.get('title').casefold() == book_title.casefold():
       return book
-  raise HTTPException(status_code=404, detail="Book not found")
