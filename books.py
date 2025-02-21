f86a4622f72dff62106ac77a21a71ac340f7c890
Nimira43 <n.rabone72@aol.com>
2025-02-21 21:03:44 +0000

    Given the name of the project I've added two very relevant books to the list!!!

--- books.py
+++ books.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title': 'The Lord of the Rings', 'author': 'JRR Tolkien', 'category': 'Fantasy'},
+    {'title': 'Nineteen Eighty-Four', 'author': 'George Orwell', 'category': 'Dystopian'},
+    {'title': 'The Lion, the Witch and the Wardrobe', 'author': 'CS Lewis', 'category': 'Fantasy'},
+    {'title': 'Dune', 'author': 'Frank Herbert', 'category': 'Science Fiction'},
+    {'title': 'War and Peace', 'author': 'Leo Tolstoy', 'category': 'Historical'},
+    {'title': 'Wuthering Heights', 'author': 'Emily Brontë', 'category': 'Gothic'},
+    {'title': 'The Odyssey', 'author': 'Homer', 'category': 'Epic'},
+    {'title': '2001: A Space Odyssey', 'author': 'Arthur C. Clarke', 'category': 'Science Fiction'},
+]
+
 @app.get('/api-endpoint')
 async def first_api():
   return {"Hello FastAPI World!"}
