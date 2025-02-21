b9961e9da4531cf934a5203bc130e5e656b8e936
Nimira43 <n.rabone72@aol.com>
2025-02-21 21:26:24 +0000

    Changed endpoint name to /books and rename the function to read_all_books.

--- books.py
+++ books.py
@@ -14,6 +14,6 @@ BOOKS = [
     {'title': '2001: A Space Odyssey', 'author': 'Arthur C. Clarke', 'category': 'Science Fiction'},
 ]
 
-@app.get('/api-endpoint')
-async def first_api():
+@app.get('/books')
+async def read_all_books():
   return BOOKS
